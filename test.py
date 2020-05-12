9ab550e38f7cb12c29dcdf9b0403e43b7799ba06
Mauricio Degregori <degregori2@gmail.com>
2020-05-12 02:13:09 -0500

    all lecture notes

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-print('testing testing!')
+# Print out all of the strings in the following array in alphabetical order sorted by the _middle_ letter of each string, each on a separate line. If the word has an even number of letters, choose the later letter, i.e. the one closer to the end of the string.  
+#     3
+#  1     5
+
+# 3 1 5
+
+# def sort_array(arr):
+#     middle = len(arr)// 2
+#     arr.sort(key=)
+#     for i in arr:
+#         print(i)
+
+
+# arr = ['Waltz', 'Tango', 'Viennese Waltz', 'Foxtrot', 'Cha Cha', 'Samba', 'Rumba', 'Paso Doble', 'Jive']
+# sort_array(arr)
+print('test')
